9cdfa0e7d4ecf41176f79bfddbf567d0becbe17a
ASJ THILAK <78794112+ASJTHILAK@users.noreply.github.com>
2021-04-29 00:45:43 +0530

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,30 @@
+package matrix;
+
+public class Matrix {
+
+    public static void main(String args[]){  
+    
+        int a[][]={{1,3,4},{2,4,3},{3,4,5}};    
+        int b[][]={{1,3,4},{2,4,3},{1,2,4}};        
+        int c[][]=new int[3][3];   
+    
+    //ADDITION OF 2 MATRIX 
+        System.out.println("MATRIX ADDITION");
+        for(int i=0;i<3;i++){    
+            for(int j=0;j<3;j++){    
+                c[i][j]=a[i][j]+b[i][j];     
+                System.out.print(c[i][j]+" ");    
+            }    
+            System.out.println();    
+        }
+    //MULTIPLICATION OF 2 MATRIX
+        System.out.println("MATRIX MULTIPLICATION");
+        for(int i=0;i<3;i++){    
+            for(int j=0;j<3;j++){    
+                c[i][j]=a[i][j]*b[i][j];     
+                System.out.print(c[i][j]+" ");    
+            }    
+            System.out.println();    
+        }
+    }
+}
